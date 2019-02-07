623233783ce0e3ec9cad8c91d41889f3c6e2a033
evgeny-vetrov <e.v.vetrov@gmail.com>
2019-02-07 21:46:15 +0300

    support temlates in product options files

--- upload_options.py
+++ upload_options.py
@@ -9,21 +9,18 @@ token = open('token.txt').read()
 store_id = 16157264
 
 
-
-
 option_files = [join('options',f) for f in listdir('options') if isfile(join('options', f))]
 for option_file in option_files:
     option_json = json.loads(open(option_file).read())
-    product_ids = option_json['product_ids']
-    data = option_json['data']
-    data = {
-            "options" : data
-           }
-    for product_id in product_ids:
-        # todo send for each order options
-        request_url = 'https://app.ecwid.com/api/v3/{store_id}/products/{id}?token={token}'.format(store_id=store_id, token=token, id=product_id)
-        #request_url = 'https://app.ecwid.com/api/v3/{store_id}/products/{id}'.format(store_id=store_id, id=product_id)
-        print(request_url)
-        resp = requests.put(request_url, json=data)
-        print(resp.text)
-        print('product_id:{} respose:{}'.format(product_id, resp.status_code))
+    templates = option_json['templates']
+    for group in option_json['groups']:
+        product_ids = group['product_ids']
+        template_ids = data['template_ids']
+        data = []
+        for tid in template_ids:
+            data.extend(templates[tid])
+        for product_id in product_ids:
+            request_url = 'https://app.ecwid.com/api/v3/{store_id}/products/{id}?token={token}'.format(store_id=store_id, token=token, id=product_id)
+            resp = requests.put(request_url, json=data)
+            print('product_id:{} respose:{}'.format(product_id, resp.status_code))
+
